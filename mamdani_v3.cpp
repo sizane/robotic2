2bcbd6b3db09da037d35d0db6210eda1fe529a0c
sizane <zane0zero@gmail.com>
2026-03-01 13:50:11 +0700

    detail kecil

--- mamdani_v3.cpp
+++ mamdani_v3.cpp
@@ -249,9 +249,11 @@ void loop() {
                 lcd.print("KANAN ");
                 break;
             case MUNDUR:
-                lcd.setCursor(0, 1);
+                lcd.setCursor(0, 0);
                 lcd.print("MUNDUR");
                 break;
         }
     }
+    delay(1000);
+    lcd.clear();
 }
